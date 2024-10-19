543dcbca2dde1194ee5ebc91cf41cd6c317b307b
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-10-19 17:07:25 +0530

    updated controller logic

---
+++ src/controllers/studentController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { StudentService } from "../services/studentService";
+
+const studentService = new StudentService();
+
+export async function getStudents(req: Request, res: Response) {
+    try {
+        const students = await studentService.getStudents();
+        res.render('students', { students });
+    } catch (error) {
+        res.status(500).json('Error fetching students');
+    }
+}
+
+export async function addStudent(req: Request, res: Response): Promise<void> {
+    try {
+        const { name, age, grade, email } = req.body;
+        await studentService.addStudent({ name, age, grade, email });
+        res.redirect('/')
+    } catch (error) {
+        res.status(500).json('Error adding student');
+    }
+}
+
+export async function updateStudent(req: Request, res: Response): Promise<void> {
+    try {
+        const { id } = req.params;
+        const { name, age, grade, email } = req.body;
+        await studentService.updateStudent(id, { name, age, grade, email })
+        res.redirect('/')
+    } catch (error) {
+        res.status(500).send('Error updating student');
+    }
+}
+
+export async function deleteStudent(req: Request, res: Response): Promise<void> {
+    try {
+        const { id } = req.params
+        await studentService.deleteStudent(id)
+        res.redirect('/')
+    } catch (error) {
+        res.status(500).send('Error deleting student');
+    }
+}
